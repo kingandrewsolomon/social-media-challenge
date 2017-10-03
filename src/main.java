a094649daeecdcf984ceb1dc2deb40fc3b156b4f
Christian De La Torre <delatorre@Christians-MacBook-Pro.local>
2017-10-02 20:42:48 -0700

    Program can take in one command line argument

--- src/main.java
+++ src/main.java
@@ -1,4 +1,8 @@
 
 public class main {
+    public static void main (String args[]) {
+        System.out.println("Hello World");
+        System.out.println("Your first argument is: " + args[0] + " " + args[1]);
+    }
 
 }

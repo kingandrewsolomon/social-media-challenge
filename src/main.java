b72b06dbdc93ae630700918586604b436fd52020
Andrew Solomon <andrewsolomon610@gmail.com>
2017-10-06 15:01:31 -0700

    Delete everything, start from scratch

--- src/main.java
+++
@@ -1,8 +0,0 @@
-
-public class main {
-    public static void main (String args[]) {
-        System.out.println("Hello World");
-        System.out.println("Your first argument is: " + args[0] + " " + args[1]);
-    }
-
-}
